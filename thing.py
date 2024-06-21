cd9c2417424ed70187b9776aa7febee948ec3234
TheDingoGuy <rbarnitz@hotmail.com>
2024-06-21 13:24:06 -0400

    this is the project

---
+++ thing.py
@@ -0,0 +1,47 @@
+import random
+noun = random.randint(1,5)
+verb = random.randint(1,5)
+word = random.randint(1,2)
+another = random.randint(1,2)
+if noun == 1:
+    print ("A dog")
+if noun == 2:
+    print ("A cat")
+if noun == 3:
+    print ("A car")
+if noun == 4:
+    print ("A bird")
+if noun == 5:
+    print ("A dinosaur")
+if verb == 1:
+    print ("walked")
+if verb == 2:
+    print ("jumped")
+if verb == 3:
+    print ("flew")
+if verb == 4:
+    print ("drove")
+if verb == 5:
+    print ("destroyed")
+    if another == 1:
+        print ("a house")
+    if another == 2:
+        print ("a car")
+if word == 1:
+    print("The end.")
+if word == 2:
+    print("and")
+    print("then")
+    print("he")
+    new_verb = random.randint(1,5)
+    if new_verb == 1:
+        print ("ran")
+    if new_verb == 2:
+        print ("ate")
+    if new_verb == 3:
+        print ("crashed")
+    if new_verb == 4:
+        print ("played")
+    if new_verb == 5:
+        print ("read")
+
